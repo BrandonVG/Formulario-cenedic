e31e6061a975e05daf09a053b1c1805e605d4614
BrandonVG <54459465+BrandonVG@users.noreply.github.com>
2020-04-02 13:30:32 -0600

    Modificaciones menores

--- main.js
+++ main.js
@@ -1,56 +1,7 @@
-var fNombre = document.forms["formulario"]["nombre"];
-var fApellido = document.forms["formulario"]["apellido"];
-var fTelefono = document.forms["formulario"]["telefono"];
-var email = document.forms["formulario"]["email"];
-var fContraseña = document.forms["formulario"]["contraseña"];
-var emVal = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-var letrasVal = /^[A-Za-z]+$/;
-var confirmarContraseña = document.forms["formulario"]["confirmarContra"];
 var enviar = document.querySelector("#enviar");
 var tabla = document.querySelector("#tabla");
 var formulario = document.querySelector("#formulario")
 var vector = JSON.parse(localStorage.getItem("xd"));
-// funcion para validar
-function validar(){
-    if (fNombre.value== ""|| !fNombre.value.match(letrasVal))
-    {
-        document.querySelector("#errorNom").innerHTML="El campo esta vacio o se introdujeron caracteres no permitidos";
-        fNombre.focus(); 
-    }
-    else{document.querySelector("#errorNom").innerHTML="";}
-    if (fApellido.value =="" ||!fApellido.value.match(letrasVal))
-    {
-        document.querySelector("#errorApellido").innerHTML="El campo esta vacio o se introdujeron caracteres no permitidos";
-        fApellido.focus();
-    }
-    else{document.querySelector("#errorApellido").innerHTML="";}
-    if (fTelefono.value == "" ||isNaN (fTelefono.value) || fTelefono.value.length >10){
-        document.querySelector("#errorTelefono").innerHTML="El campo esta vacio o es un numero incorrecto";
-        fTelefono.focus();
-    }
-    else{document.querySelector("#errorTelefono").innerHTML="";}
-    if (email.value =="" ||!email.value.match(emVal))
-    {
-        document.querySelector("#errorEmail").innerHTML="El correo no es valido o esta vacio";
-        email.focus();
-    }
-    else{document.querySelector("#errorEmail").innerHTML="";}
-    if (fContraseña.value=="" )
-    {
-        document.querySelector("#errorContra").innerHTML="Contraseña no valida o el campo esta vacio ";
-        fContraseña.focus();
-    }
-    else {document.querySelector("#errorContra").innerHTML="";}
-    if (confirmarContraseña.value=="" || confirmarContraseña.value != fContraseña.value|| confirmarContraseña.value.length < 8 || confirmarContraseña.value.length > 16 )
-    {
-        document.querySelector("#errorConfirmar").innerHTML="Las contraseñas no son iguales o el campo esta vacio (maximo 16 y minimo 8 caracteres)";
-        confirmarContraseña.focus();
-        return false;
-    }
-    else{document.querySelector("#errorConfirmar").innerHTML="";}
-    guardar();
-    
-}
 //Funcion de guardar en ls
 function guardar(){
     
@@ -73,7 +24,7 @@ function guardar(){
         url:"recibir.php",
         data: objeto,
         success: function(data){
-            alert(data);
+            console.log(data);
         }
     });
     return true;
@@ -98,6 +49,14 @@ function pintar(){
 //Funcion para borrar
 function borrar(event){
     var id = event.getAttribute("drid");
+    $.ajax({
+        method: "POST",
+        url: "delete.php",
+        data: {"userId":id},
+        success: function(data){
+            console.log(data);
+        }
+    })
     var vector = JSON.parse(localStorage.getItem("xd"));
     vector.splice(id,1);
     localStorage.setItem("xd",JSON.stringify(vector));
@@ -106,10 +65,11 @@ function borrar(event){
 }
 //Funcion para editar
 function editar(event){
-    console.log("xd");
     var id = event.getAttribute("eid");
+    $.ajax({
+        
+    })
     var vector = JSON.parse(localStorage.getItem("xd"));
-    console.log(vector[id]);
     fNombre.value=vector[id].nombre;
     fApellido.value=vector[id].apellido;
     fTelefono.value=vector[id].telefono;
@@ -118,7 +78,6 @@ function editar(event){
     vector.splice(id,1);
     localStorage.setItem("xd",JSON.stringify(vector));
     pintar();
-    location.reload();
     if (validar()==true){
         guardar();
     }
---
+++ validar.js
@@ -0,0 +1,52 @@
+var fNombre = document.forms["formulario"]["nombre"];
+var fApellido = document.forms["formulario"]["apellido"];
+var fTelefono = document.forms["formulario"]["telefono"];
+var email = document.forms["formulario"]["email"];
+var fContraseña = document.forms["formulario"]["contraseña"];
+var emVal = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+var letrasVal = /^[A-Za-z]+$/;
+var confirmarContraseña = document.forms["formulario"]["confirmarContra"];
+function validar(){
+    if (fNombre.value== ""|| !fNombre.value.match(letrasVal))
+    {
+        document.querySelector("#errorNom").innerHTML="El campo esta vacio o se introdujeron caracteres no permitidos";
+        fNombre.focus(); 
+        return false;
+    }
+    else{document.querySelector("#errorNom").innerHTML="";}
+    if (fApellido.value =="" ||!fApellido.value.match(letrasVal))
+    {
+        document.querySelector("#errorApellido").innerHTML="El campo esta vacio o se introdujeron caracteres no permitidos";
+        fApellido.focus();
+        return false;
+    }
+    else{document.querySelector("#errorApellido").innerHTML="";}
+    if (fTelefono.value == "" ||isNaN (fTelefono.value) || fTelefono.value.length >10){
+        document.querySelector("#errorTelefono").innerHTML="El campo esta vacio o es un numero incorrecto";
+        fTelefono.focus();
+        return false;
+    }
+    else{document.querySelector("#errorTelefono").innerHTML="";}
+    if (email.value =="" ||!email.value.match(emVal))
+    {
+        document.querySelector("#errorEmail").innerHTML="El correo no es valido o esta vacio";
+        email.focus();
+        return false;
+    }
+    else{document.querySelector("#errorEmail").innerHTML="";}
+    if (fContraseña.value=="" )
+    {
+        document.querySelector("#errorContra").innerHTML="Contraseña no valida o el campo esta vacio ";
+        fContraseña.focus();
+        return false;
+    }
+    else {document.querySelector("#errorContra").innerHTML="";}
+    if (confirmarContraseña.value=="" || confirmarContraseña.value != fContraseña.value|| confirmarContraseña.value.length < 8 || confirmarContraseña.value.length > 16 )
+    {
+        document.querySelector("#errorConfirmar").innerHTML="Las contraseñas no son iguales o el campo esta vacio (maximo 16 y minimo 8 caracteres)";
+        confirmarContraseña.focus();
+        return false;
+    }
+    else{document.querySelector("#errorConfirmar").innerHTML="";}
+    guardar();
+}
